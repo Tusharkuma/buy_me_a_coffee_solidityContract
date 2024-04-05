5632cf3b1ea6fbd8563c1633eea0b7713a92a762
tusharkuma <tusharkumar121001@gmail.com>
2024-04-05 21:39:09 +0530

    deploy.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
-
-const hre = require("hardhat");
+const { ethers } = require("ethers"); // importing ethers library to file
+const hre = require("hardhat");  // importing hardhat to file
 
 async function main() {
 
@@ -9,4 +9,21 @@ async function main() {
     await Coffee.deployed();
 
     console.log("coffee contract deployed to: ", Coffee.address);
+
+    const buy = await Coffee.functions.buyCoffee({
+        value: ethers.utils.parseEther("1.0")
+    });
+
+    console.log(buy); //
+    await buy.wait(); //wait to complete buy
+    console.log(await Coffee.provider.getBalance(Coffee.address)); //show the balance in Coffee contract
+
+    const withdraw = await Coffee.functions.withdraw();
+    console.log(withdraw);
+    await withdraw.wait();
+    console.log( await Coffee.providers.getBalance(Coffee.address)); //show the balance in Coffee contract
+    
+
 }
+
+main()
